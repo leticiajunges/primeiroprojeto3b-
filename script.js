c5e16de9ce9e68b7a7a205ad01c76c3c208e0744
leticiajunges <leticia.junges@escola.pr.gov.br>
2025-06-24 12:43:37 +0000

    atualização

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-document.addEventListener('DOMContentLoaded', function ( ) {
-    let tamanhoAtualFonte=1;
+document.addEventListener('DOMContentLoaded', function () {
+    let tamanhoAtualFonte = 1;
 
-    const aumenta FonteBotao
-}
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-)
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        if (tamanhoAtualFonte < 0.5) tamanhoAtualFonte = 0.5; // Evita fonte minúscula demais
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+});
+const contrasteBotao = document.getElementById('contraste');
+
+contrasteBotao.addEventListener('click', function () {
+    document.body.classList.toggle('alto-contraste');
+});
